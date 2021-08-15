2a1e4a22db9ede9ee206af72bd14687cb33d89eb
freshLiver <freshliver.cys@gmail.com>
2021-08-15 20:10:02 +0800

    Timed Check Community Post by Using CORS Request on Holodex

--- main.js
+++ main.js
@@ -1,58 +1,35 @@
 // ==UserScript==
 // @name         Youtube-Community-Post-Notification
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Youtube Community Posts
 // @author       freshLiver
-// @match        https://www.youtube.com
-// @icon         https://www.google.com/s2/favicons?domain=youtube.com
+// @match        https://holodex.net/*
 // @grant        GM_notification
+// @grant        GM_xmlhttpRequest
 // @require      https://code.jquery.com/jquery-latest.min.js
+// @connect      www.youtube.com
 // ==/UserScript==
 
-const yt_base_url = "https://www.youtube.com";
+const msCheckInterval = 1000 * 60;
+const msNotificationDuration = 1000 * 5;
+const msNotificationMaxRepeatTimes = 3;
 
+const noNotificationAfterClicked = true;
+const openPostAfterMaxRepeat = true;
+const disableNotificationSound = false;
+const enableNotificationHighlightTab = true;
 
-function get_channel_community_posts(channel_id) {
+const channel_id_list = [
+    "UCXTpFs_3PqI41qX2d9tL2Rw"
+];
 
-    // send sync http get request and get resp text(data)
-    let community_posts = null;
-    $.ajax({
-        async: false,
-        type: 'GET',
-        url: `${yt_base_url}/channel/${channel_id}/community`,
-        success: function (data) {
-
-            // add resp text into a new element for parsing
-            const result_element = $(`<div>${data}</div>`);
-
-            // get content script from result element and extract json data
-            const content = result_element.find("link[rel=canonical]").prev(),
-                data_begin = content.text().search("="),
-                data_end = content.text().lastIndexOf(";"),
-                channel_data = JSON.parse(content.text().slice(data_begin + 1, data_end));
-
-            // extract community data
-            let community = null;
-            for (const tab of channel_data.contents.twoColumnBrowseResultsRenderer.tabs) {
-                if (tab.tabRenderer.title === '社群') {
-                    community = tab.tabRenderer;
-                    break;
-                }
-            }
-
-            // extract post list from community data and get latest post
-            community_posts = community.content.sectionListRenderer.contents[0].itemSectionRenderer.contents;
-        }
-    });
-    return community_posts;
-}
 
 function notify(channel_id, postData) {
 
     // get info from post data
     const postInfo = postData.backstagePostThreadRenderer.post.backstagePostRenderer;
-    const postVideo = postInfo.backstageAttachment.videoRenderer;
+    const postVideo = postInfo.backstageAttachment?.videoRenderer;
 
     // post info
     const info = {
@@ -71,17 +48,17 @@ function notify(channel_id, postData) {
         },
         "id": postInfo.postId,
         "time": postInfo.publishedTimeText.runs[0].text,
-        "link": `${yt_base_url}/channel/${channel_id}/community?lb=${postInfo.postId}`,
+        "link": `https://www.youtube.com/channel/${channel_id}/community?lb=${postInfo.postId}`,
     };
 
     // convert info to notification data
     const notification = {
-        title: `${info.channel.name} - (${info.time}) - ${info.content.videoInfo.title || ""}`,
+        title: `${info.time} - ${info.channel.name} ${info.content.videoInfo.title || ""}`,
         text: info.content.text,
         image: info.content.videoInfo.thumbnail || info.channel.thumbnail,
-        highlight: true,
-        silent: true,
-        timeout: 5,
+        highlight: enableNotificationHighlightTab,
+        silent: disableNotificationSound,
+        timeout: msNotificationDuration,
         onclick: () => {
             // if no video, open post page and focus to it
             const newWin = window.open(info.content.videoInfo.link || info.link);
@@ -93,22 +70,54 @@ function notify(channel_id, postData) {
     GM_notification(notification);
 }
 
+function get_channel_community_posts(channel_id) {
+
+    // send cors http get request and get resp text(data)
+    GM_xmlhttpRequest({
+        url: `https://www.youtube.com/channel/${channel_id}/community`,
+        method: 'GET',
+        responseType: 'text/html',
+        onload: (arg) => {
+            if (arg.status != 200)
+                alert(arg.statusText);
+            else {
+                // add resp text into a new element for parsing
+                const result_element = $(`<div>${arg.responseText}</div>`);
+
+                // get content script from result element and extract json data
+                const content = result_element.find("link[rel=canonical]").prev(),
+                    data_begin = content.text().search("="),
+                    data_end = content.text().lastIndexOf(";"),
+                    channel_data = JSON.parse(content.text().slice(data_begin + 1, data_end));
+
+                // extract community data
+                let community = null;
+                for (const tab of channel_data.contents.twoColumnBrowseResultsRenderer.tabs) {
+                    if (tab.tabRenderer.title === '社群') {
+                        community = tab.tabRenderer;
+                        break;
+                    }
+                }
+
+                // extract post list from community data and get latest post
+                const community_posts = community.content.sectionListRenderer.contents[0].itemSectionRenderer.contents;
+
+                // TODO : check post status
+
+                // send notification
+                notify(channel_id, community_posts[0]);
+            }
+        }
+    });
+}
+
 (function () {
     'use strict';
 
-    // check each channel's community post
-    const channel_id_list = [
-        "UCXTpFs_3PqI41qX2d9tL2Rw",
-    ];
+    // set repeat interval (in ms)
+    window.setInterval(() => {
+        for (const channel_id of channel_id_list)
+            get_channel_community_posts(channel_id);
+    }, msCheckInterval);
 
-    for (const channel_id of channel_id_list) {
-
-        // get community posts of this channel
-        const post_list = get_channel_community_posts(channel_id);
-
-        // get post info
-        notify(channel_id, post_list[1]);
-    }
-
-    alert("resp done.");
 })();
